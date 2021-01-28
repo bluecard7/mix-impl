04828dd427b85a4799ea24adf4653ddefd0df1ce
Paul Yang <paul.junho.yang@gmail.com>
2021-01-27 17:38:28 -0800

    added load negative and store insts

--- insttemplates.go
+++ insttemplates.go
@@ -8,21 +8,20 @@ func (dst InstTemplates) merge(src InstTemplates) {
 	}
 }
 
-func LD(machine *MIXArch, inst *Instruction, rIdx int) {
-	contents := machine.ReadCell(inst.A())
+func LD(inst *Instruction, dst Register, content []MIXByte) {
 	L, R := inst.F()
-	machine.R[rIdx][0] = POS_SIGN
+	dst[0] = POS_SIGN
 	if L == 0 {
-		machine.R[rIdx][0] = contents[0]
+		dst[0] = content[0]
 		L = 1
 	}
-	partial := contents[L : R+1]
-	copy(machine.R[rIdx][6-len(partial):], partial)
+	partial := content[L : R+1]
+	copy(dst[len(dst)-len(partial):], partial)
 }
 
 // load insts, #8 - 15
-var load = func() InstTemplates {
-	load := make(InstTemplates)
+func loads() InstTemplates {
+	templates := make(InstTemplates)
 	entries := []struct {
 		Name string
 		R    int
@@ -37,127 +36,114 @@ var load = func() InstTemplates {
 		{"LDX", X},
 	}
 	for i, entry := range entries {
-		load[entry.Name] = func(codeOffset, regI int) func() *Instruction {
+		templates[entry.Name] = func(codeOffset, regI int) func() *Instruction {
 			return func() *Instruction {
 				return &Instruction{
 					Code: baseInstCode(0, 5, MIXByte(8+codeOffset)),
 					Exec: func(machine *MIXArch, inst *Instruction) {
-						LD(machine, inst, regI)
+						content := machine.ReadCell(inst.A())
+						LD(inst, machine.R[regI], content)
 					},
 				}
 			}
 		}(i, entry.R)
 	}
-	return load
-}()
-
-/*
-InstTemplates{
-	//"LDAN": nil, #16
-	//"LDXN": nil, #23
-	//"LDiN": nil, #17-22
+	return templates
 }
-*/
 
-/*
-func ST(machine *MIXArch, inst *Instruction, register []MIXByte) {
+// load negative insts, #16 - 23
+func loadNs() InstTemplates {
+	templates := make(InstTemplates)
+	entries := []struct {
+		Name string
+		R    int
+	}{
+		{"LDAN", A},
+		{"LD1N", I1},
+		{"LD2N", I2},
+		{"LD3N", I3},
+		{"LD4N", I4},
+		{"LD5N", I5},
+		{"LD6N", I6},
+		{"LDXN", X},
+	}
+	for i, entry := range entries {
+		templates[entry.Name] = func(codeOffset, regI int) func() *Instruction {
+			return func() *Instruction {
+				return &Instruction{
+					Code: baseInstCode(0, 5, MIXByte(16+codeOffset)),
+					Exec: func(machine *MIXArch, inst *Instruction) {
+						content := machine.ReadCell(inst.A())
+						switch content[0] {
+						case POS_SIGN:
+							content[0] = NEG_SIGN
+						case NEG_SIGN:
+							content[0] = POS_SIGN
+						}
+						LD(inst, machine.R[regI], content)
+					},
+				}
+			}
+		}(i, entry.R)
+	}
+	return templates
+}
+
+func ST(machine *MIXArch, inst *Instruction, r Register) {
 	L, R := inst.F()
-	if len(register) < 6 { // in case of index or jump register
-		register = append([]MIXByte{register[0], 0, 0, 0}, register[1:]...)
+	if len(r) < 6 { // in case of index or jump register
+		r = append([]MIXByte{r[0], 0, 0, 0}, r[1:]...)
 	}
 	content := machine.ReadCell(inst.A())
 	if L == 0 { // if sign is included
-		content[0] = register[0]
+		content[0] = r[0]
 		L = 1
 	}
-	copy(content[L:R+1], register[6-(R-L+1):])
+	copy(content[L:R+1], r[6-(R-L+1):])
 	machine.WriteCell(inst.A(), content)
 }
 
-var store = InstTemplates{
-	"STA": func() *Instruction {
-		return &Instruction{
-			Code: baseInstCode(0, 5, 24),
-			Exec: func(machine *MIXArch, inst *Instruction) {
-				ST(machine, inst, machine.Regs.A)
-			},
-		}
-	},
-	"ST1": func() *Instruction {
-		return &Instruction{
-			Code: baseInstCode(0, 5, 25),
-			Exec: func(machine *MIXArch, inst *Instruction) {
-				ST(machine, inst, machine.Regs.I1)
-			},
-		}
-	},
-	"ST2": func() *Instruction {
-		return &Instruction{
-			Code: baseInstCode(0, 5, 26),
-			Exec: func(machine *MIXArch, inst *Instruction) {
-				ST(machine, inst, machine.Regs.I2)
-			},
-		}
-	},
-	"ST3": func() *Instruction {
-		return &Instruction{
-			Code: baseInstCode(0, 5, 27),
-			Exec: func(machine *MIXArch, inst *Instruction) {
-				ST(machine, inst, machine.Regs.I3)
-			},
-		}
-	},
-	"ST4": func() *Instruction {
-		return &Instruction{
-			Code: baseInstCode(0, 5, 28),
-			Exec: func(machine *MIXArch, inst *Instruction) {
-				ST(machine, inst, machine.Regs.I4)
-			},
-		}
-	},
-	"ST5": func() *Instruction {
-		return &Instruction{
-			Code: baseInstCode(0, 5, 29),
-			Exec: func(machine *MIXArch, inst *Instruction) {
-				ST(machine, inst, machine.Regs.I5)
-			},
-		}
-	},
-	"ST6": func() *Instruction {
-		return &Instruction{
-			Code: baseInstCode(0, 5, 30),
-			Exec: func(machine *MIXArch, inst *Instruction) {
-				ST(machine, inst, machine.Regs.I6)
-			},
-		}
-	},
-	"STX": func() *Instruction {
-		return &Instruction{
-			Code: baseInstCode(0, 5, 31),
-			Exec: func(machine *MIXArch, inst *Instruction) {
-				ST(machine, inst, machine.R[X])
-			},
-		}
-	},
-	"STJ": func() *Instruction {
-		return &Instruction{
-			Code: baseInstCode(0, 5, 32),
-			Exec: func(machine *MIXArch, inst *Instruction) {
-			},
-		}
-	},
-	"STZ": func() *Instruction {
-		return &Instruction{
-			Code: baseInstCode(0, 5, 33),
-			Exec: func(machine *MIXArch, inst *Instruction) {
-			},
-		}
-	},
+func stores() InstTemplates {
+	entries := []struct {
+		Name string
+		R    int
+	}{
+		{"STA", A},
+		{"ST1", I1},
+		{"ST2", I2},
+		{"ST3", I3},
+		{"ST4", I4},
+		{"ST5", I5},
+		{"ST6", I6},
+		{"STX", X},
+		{"STJ", J},
+		{"STZ", A},
+	}
+	templates := make(InstTemplates)
+	for i, entry := range entries {
+		templates[entry.Name] = func(codeOffset, regI int) func() *Instruction {
+			return func() *Instruction {
+				return &Instruction{
+					Code: baseInstCode(0, 5, MIXByte(24+codeOffset)),
+					Exec: func(machine *MIXArch, inst *Instruction) {
+						r := machine.R[regI]
+						if entry.Name == "STZ" {
+							r = Register(NewWord())
+						}
+						ST(machine, inst, r)
+					},
+				}
+			}
+		}(i, entry.R)
+	}
+	return templates
 }
-*/
+
 func aggregateTemplates() InstTemplates {
 	templates := make(InstTemplates)
-	templates.merge(load)
+	templates.merge(loads())
+	templates.merge(loadNs())
+	templates.merge(stores())
 	return templates
 }
 
